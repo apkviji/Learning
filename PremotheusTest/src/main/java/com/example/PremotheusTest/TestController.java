204d7b5582b27029e360a516ad72c03493eecedf
apkviji <apkviji@gmail.com>
2025-09-17 21:45:23 +0530

    add job in prometheus.yml

---
+++ PremotheusTest/src/main/java/com/example/PremotheusTest/TestController.java
@@ -0,0 +1,12 @@
+package com.example.PremotheusTest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public String test(){
+        return "Welcome to PremotheusTest";
+    }
+}
